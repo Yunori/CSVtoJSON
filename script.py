400a21f76f8ebd3612cfd94591e3df5eebe953c4
Yunori <bertocco.tk@gmail.com>
2017-11-22 14:43:00 +0100

    sort by CP and added dtype + encoding

--- script.py
+++ script.py
@@ -8,57 +8,54 @@ Created on Tue Nov 14 14:33:52 2017
 import json
 import pandas as pd
 
-def parsecsv(dpts, dataset):
-    """Parse un csv et renvoie un dictionnaire trié."""
+dataset = pd.read_csv('Dataset.csv', dtype=object, encoding='cp850')
+dpts = pd.read_csv('Departements.csv', dtype=object, encoding='utf_8')
+dataset = dataset.sort_index(1, ascending=False)
+tailledpt = len(dpts)
+counter1 = 0
+listedpts = {}
 
-    tailledpt = len(dpts)
-    counter1 = 0
-    listedpts = {}
+while counter1 < tailledpt:
+    listedpts[dpts.iloc[counter1, 1]] = dpts.iloc[counter1, 2]
+    counter1 += 1
 
-    while counter1 < tailledpt:
-        listedpts[dpts.iloc[counter1, 1]] = dpts.iloc[counter1, 2]
-        counter1 += 1
+tailledata = len(dataset)
+counter2 = 0
+final = {}
 
-    tailledata = len(dataset)
-    counter2 = 0
-    final = {}
+while counter2 < tailledata:
+    codepostal = str(dataset.iloc[counter2, 1])
 
-    while counter2 < tailledata:
-        codepostal = str(dataset.iloc[counter2, 0])
+    if codepostal[:2] == '97' or codepostal[:2] == '98':
+        codedpt = str(dataset.iloc[counter2, 1])[:3]
+    else:
+        codedpt = str(dataset.iloc[counter2, 1])[:2]
+    if codedpt == '20':
+        codedpt = '2a'
+    elif codedpt == '21':
+        codedpt = '2b'
+    elif codepostal == '42620':
+        codedpt = '03'
+    elif codepostal == '05110':
+        codedpt = '04'
+    elif codepostal == '33220':
+        codedpt = '24'
 
-        if codepostal[:2] == '97' or codepostal[:2] == '98':
-            codedpt = str(dataset.iloc[counter2, 0])[:3]
-        else:
-            codedpt = str(dataset.iloc[counter2, 0])[:2]
-        if codedpt == '20':
-            codedpt = '2a'
-        elif codedpt == '21':
-            codedpt = '2b'
-        elif codepostal == '42620':
-            codedpt = '03'
-        elif codepostal == '05110':
-            codedpt = '04'
-        elif codepostal == '33220':
-            codedpt = '24'
+    nomdpt = listedpts[codedpt]
+    nomville = dataset.iloc[counter2, 1]
+    adresse = dataset.iloc[counter2, 2]
 
-        nomdpt = listedpts[codedpt]
-        nomville = dataset.iloc[counter2, 1]
-        adresse = dataset.iloc[counter2, 2]
-
-        if nomdpt not in final:
-            final[nomdpt] = {}
-        if nomville not in final[nomdpt]:
-            final[nomdpt][nomville] = {}
-        indexcounter1 = len(final[nomdpt][nomville])
-        final[nomdpt][nomville][indexcounter1] = {}
-        final[nomdpt][nomville][indexcounter1]['codepostal'] = codepostal
-        final[nomdpt][nomville][indexcounter1]['departement'] = nomdpt
-        final[nomdpt][nomville][indexcounter1]['ville'] = nomville
-        final[nomdpt][nomville][indexcounter1]['adresse'] = adresse
-        counter2 += 1
-
-    return final
-
+    if nomdpt not in final:
+        final[nomdpt] = {}
+    if nomville not in final[nomdpt]:
+        final[nomdpt][nomville] = {}
+    indexcounter1 = len(final[nomdpt][nomville])
+    final[nomdpt][nomville][indexcounter1] = {}
+    final[nomdpt][nomville][indexcounter1]['codepostal'] = codepostal
+    final[nomdpt][nomville][indexcounter1]['departement'] = nomdpt
+    final[nomdpt][nomville][indexcounter1]['ville'] = nomville
+    final[nomdpt][nomville][indexcounter1]['adresse'] = adresse
+    counter2 += 1
 
 with open('data.json', 'w') as outfile:
-    json.dump(parsecsv(pd.read_csv('Departements.csv'), pd.read_csv('Dataset.csv')), outfile)
+    json.dump(final, outfile)
